102901c8be330b39666e059cce6984d81eb04818
collinsoden22 <collinsoden22@gmail.com>
2023-01-24 09:27:27 +0100

    Exported task 0

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,13 @@
-const taskFirst = () => {
+export const taskFirst = () => {
   const task = 'I prefer const when I can.';
   return task;
 };
 
-const getLast = () => ' is okay';
+export const getLast = () => ' is okay';
 
-const taskNext = () => {
+export const taskNext = () => {
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 };
-
-console.log(`${taskFirst()} ${taskNext()}`);

d6cbbc9f47f4df62ccb926f45ffd028d4715be50
collinsoden22 <collinsoden22@gmail.com>
2023-01-24 12:26:08 +0100

    Fixed bug

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,7 +1,3 @@
 const getSumOfHoods = (a, b = 89, c = 19) => a + b + c;
 
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
-
 export default getSumOfHoods;

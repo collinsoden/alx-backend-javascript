27cbf979f9c11fedfd482c38f9e9243a6160c555
collinsoden22 <collinsoden22@gmail.com>
2023-01-24 11:31:42 +0100

    Added task 1

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -2,7 +2,3 @@ import taskBlock from './1-block-scoped.js';
 
 console.log(taskBlock(true));
 console.log(taskBlock(false));
-
-import { taskFirst, taskNext } from './0-constants.js';
-
-console.log(`${taskFirst()} ${taskNext()}`);

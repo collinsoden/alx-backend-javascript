8d34f5d9f71371ab17adf254379c35b948b3471d
collinsoden22 <collinsoden22@gmail.com>
2023-01-24 12:24:59 +0100

    Added task 3

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,7 @@
-import getSumOfHoods from './3-default-parameter.js';
+const getSumOfHoods = (a, b = 89, c = 19) => a + b + c;
 
 console.log(getSumOfHoods(34));
 console.log(getSumOfHoods(34, 3));
 console.log(getSumOfHoods(34, 3, 4));
+
+export default getSumOfHoods;

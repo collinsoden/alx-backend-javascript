257712fa2f3138f147104e2e14a67c5d84b1da45
collinsoden22 <collinsoden22@gmail.com>
2023-01-24 11:51:58 +0100

    Added task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,10 @@ const taskBlock = (trueOrFalse) => {
   let task = false;
   let task2 = true;
 
-  if (trueOrFalse) {
-    () => {
-      task = true;
-      task2 = false;
-    };
+  if (trueOrFalse) () => {
+    task = true;
+    task2 = false;
   }
-
   return [task, task2];
 };
 

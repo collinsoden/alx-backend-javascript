6f986d153cf2c134342ceb6562d78c0dfdfe4674
collinsoden22 <collinsoden22@gmail.com>
2023-01-31 20:29:45 +0100

    Data manipulation tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,18 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-<<<<<<< HEAD
-  let newArray = {};
-  for (const i of array) {
-    const thevalue = array[i];
-    [`${appendString} ${thevalue}`];
-  }
 
-  return newArray;
-=======
   const returnArray = [];
   for (const element of array) {
     returnArray.push(appendString + element);
   }
 
   return returnArray;
->>>>>>> 85113e7f3da3e4cc8100332475190bee0c9f300a
+
 }
